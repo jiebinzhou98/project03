834c5dbcbf4463f485bb89dc0f7655780495df9b
jiebinzhou98 <benzhou98@outlook.com>
2025-01-05 16:36:41 -0500

    perf: :sparkles: including new features

--- src/main.js
+++ src/main.js
@@ -16,6 +16,7 @@ class GameScene extends Phaser.Scene{
     this.cursor;
     this.playerSpeed = speedDown + 50;
     this.target
+    this.points = 0;
   }
 
   preload(){
@@ -33,10 +34,15 @@ class GameScene extends Phaser.Scene{
     this.player.body.allowGravity = false;
     this.player.setCollideWorldBounds(true);
 
+    this.player.setSize(this.player.width-this.player.width/4, this.player.height/6).
+    setOffset(this.player.width/10, this.player.height - this.player.height/10);
+
     this.target = this.physics.add
     .image(0, 0, "apple")
     .setOrigin(0, 0)
     this.target.setMaxVelocity(0, speedDown);
+
+    this.physics.add.overlap(this.target, this.player, this.targetHit, null, this)
 
     this.cursor = this.input.keyboard.createCursorKeys();
   }
@@ -45,6 +51,7 @@ class GameScene extends Phaser.Scene{
 
     if(this.target.y >= sizes.height){
       this.target.setY(0);
+      this.target.setX(this.getRandomX())
     }
 
     const {left, right} = this.cursor;
@@ -56,6 +63,17 @@ class GameScene extends Phaser.Scene{
     }else{
       this.player.setVelocityX(0)
     }
+  }
+
+
+  getRandomX(){
+    return Math.floor(Math.random() * 480);
+  }
+
+  targetHit() {
+    this.target.setY(0);
+    this.target.setX(this.getRandomX());
+    this.points++;
   }
 }
 
