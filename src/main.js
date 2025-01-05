14d2a45ced78dd805b5f103d62d0672922f1e5dc
jiebinzhou98 <benzhou98@outlook.com>
2025-01-05 16:19:31 -0500

    perf: :zap: implement player movement

--- src/main.js
+++ src/main.js
@@ -12,7 +12,10 @@ const speedDown = 300
 class GameScene extends Phaser.Scene{
   constructor(){
     super("scene-game")
-    this.player
+    this.player;
+    this.cursor;
+    this.playerSpeed = speedDown + 50;
+    this.target
   }
 
   preload(){
@@ -21,11 +24,28 @@ class GameScene extends Phaser.Scene{
   }
   
   create(){
-    this.add.image(0,0,"bg").setOrigin(0,0)
-    this.player = this.physics.add.image(0, sizes.height-100,"basket").setOrigin(0,0)
+    this.add.image(0,0,"bg").setOrigin(0,0);
+    this.player = this.physics.add
+      .image(0, sizes.height - 100, "basket")
+      .setOrigin(0, 0)
+    this.player.setImmovable(true);
+    this.player.body.allowGravity = false;
+    this.player.setCollideWorldBounds(true);
+
+    this.cursor = this.input.keyboard.createCursorKeys();
   }
 
-  update(){}
+  update(){
+    const {left, right} = this.cursor;
+
+    if(left.isDown){
+      this.player.setVelocityX(-this.playerSpeed);
+    }else if(right.isDown){
+      this.player.setVelocityX(this.playerSpeed);
+    }else{
+      this.player.setVelocityX(0)
+    }
+  }
 }
 
 const config = {
