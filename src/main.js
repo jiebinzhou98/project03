b4f4eb83d2dc55258f4190b09c9ac6934996127c
jiebinzhou98 <benzhou98@outlook.com>
2025-01-05 15:42:57 -0500

    feat: :sparkles: load the bg image to the gameCanvas

--- src/main.js
+++ src/main.js
@@ -1,16 +1,43 @@
 import './style.css'
-import Phaser from 'phaser'
+import Phaser, { Physics } from 'phaser'
+
 
 const sizes ={
   width:500,
   height:500
 }
 
+const speedDown = 300
+
+class GameScene extends Phaser.Scene{
+  constructor(){
+    super("scene-game")
+  }
+
+  preload(){
+    this.load.image("bg", "/assets/bg.png")
+  }
+  
+  create(){
+    this.add.image(0,0,"bg").setOrigin(0,0)
+  }
+
+  update(){}
+}
+
 const config = {
   type: Phaser.WEBGL,
   width:sizes.width,
   height: sizes.height,
-  canvas:gameCanvas
+  canvas:gameCanvas,
+  physics:{
+    default:"arcade",
+    arcade:{
+      gravity:{y:speedDown},
+      debug: true
+    }
+  },
+  scene: [GameScene]
 }
 
 
